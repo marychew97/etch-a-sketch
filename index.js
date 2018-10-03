8158b4a1ba48a8eaf35ff3acade0b2508674132a
Mary Chew <jiayi1172@gmail.com>
2018-10-03 21:10:02 +0800

    Updated

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 let container = document.querySelector('.container');
-let width = 480;
-let height = 480;
+document.getElementById('reset').addEventListener('click', reset);
 
-let num = prompt("Enter any number for grid dimensions.");
+let oriNum = 16;
 
-// function createGrid(num){
-    
-//     for(var row=0; row<num; row++){
-//         var row = document.createElement('div');
-//         row.classList.add('row');
-//         for(var col=1; col<=num; col++){
-//             var cell = document.createElement('div');
-//             cell.classList.add('cell');
-//             cell.style.width = `${480/num}px`;
-//             cell.style.height = `${480/num}px`;
-//             row.appendChild(cell);
-//         }
-//         container.appendChild(row);
-//     }
-//     // cell.width(480/num);
-//     // cell.height(480/num);
-// }
-
-// createGrid(16);
 
 function createGrid(num){
-    //var body = document.querySelector('body');
     var table = document.createElement('table');
     var tableBody = document.createElement('tbody');
     table.appendChild(tableBody);
@@ -34,12 +13,41 @@ function createGrid(num){
         tableBody.appendChild(tr);
         for(var j=0; j<num; j++){
             var td = document.createElement('td');
-            td.style.width = `${width/num}px`;
-            td.style.height = `${height/num}px`;
+            td.classList.add('cell');
+            td.addEventListener('mouseover', function(e){
+                e.target.style.backgroundColor = getRandomColor();
+            });
+            td.style.width = `${480/num}px`;
+            td.style.height = `${480/num}px`;
             tr.appendChild(td);
         }
         container.appendChild(table);
     }
 }
 
-createGrid(num);
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for(var i=0; i<6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function clearGrid(){
+    var table = document.querySelector('table');
+    container.removeChild(table);
+}
+
+function reset(){
+    var td = document.querySelectorAll('td');
+    td.forEach(data => {
+        data.style.backgroundColor = '#fff';
+    });
+
+    let num = prompt("Enter any number for grid dimensions.");
+    clearGrid();
+    createGrid(num);
+}
+
+createGrid(oriNum);
